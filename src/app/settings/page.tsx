dbe08ebf4e2971a40ef977245d270921c77356d9
Devin Garcia <devingarcia5423@gmail.com>
2024-01-26 14:02:48 -0500

    forgot to make Settings route a module

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
+import { Metadata } from 'next';
+import Header from '@/components/Header';
+import CourseSelector from '@/components/CourseSelector';
+import NavBar from '@/components/NavBar';
+
+export const metadata: Metadata = {
+	title: 'Setttings | Polyglot',
+	description: 'Your passport to global communication!',
+	openGraph: {
+		title: 'Setttings | Polyglot',
+		description: 'Your passport to global communication!',
+	},
+	twitter: {
+		title: 'Setttings | Polyglot',
+		description: 'Your passport to global communication!',
+	},
+};
+
+export default function Setttings() {
+	return (
+		<main className='flex max-h-[100svh] max-w-[100vw] overflow-visible flex-col items-center justify-between '>
+			<Header text={'Kathryn'} profile={true} />
+			<section className='h-[calc(100svh-8rem)] w-full px-5 py-2 flex flex-col gap-3'>
+				<CourseSelector />
+			</section>
+			<NavBar />
+		</main>
+	);
+}
